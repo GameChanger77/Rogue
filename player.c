cf9fd692154b9f9124a4ac1575ccb94f142181fc
GameChanger77 <gamer4christ2004@gmail.com>
2022-12-02 15:04:30 -0600

    Collision Detection

--- player.c
+++ player.c
@@ -2,23 +2,37 @@
 
 #include "player.h"
 #include "input.h"
+#include "roomgen.h"
 
 
 void move_character(Direction direction, Player* player) {
+	int desiredX = player->room_x;
+	int desiredY = player->room_y;
+	
 	switch (direction) {
 		case Up:
-			player->room_y-=1;
+			desiredY-=1;
 			break;
 		case Down:
-			player->room_y+=1;
+			desiredY+=1;
 			break;
 		case Left:
-			player->room_x-=1;
+			desiredX-=1;
 			break;
 		case Right:
-			player->room_x+=1;
+			desiredX+=1;
 			break;
 		default:
 			break;
+	}
+	
+	//mvprintw(1, 1, "% 4d", desiredX);
+	//mvprintw(2, 1, "% 4d", desiredY);
+	//mvprintw(3, 1, "Tile = %d  ", room_get_tiles(roomgen_get(0,0))[0][desiredY][desiredX]);
+	//mvprintw(4, 1, "Tile = %c  ", room_get_tiles(roomgen_get(0,0))[0][desiredY][desiredX]);
+	
+	if (room_get_tiles(roomgen_get(0,0))[0][desiredY][desiredX] != '#'){
+		player->room_x=desiredX;
+		player->room_y=desiredY;
 	}
 }
