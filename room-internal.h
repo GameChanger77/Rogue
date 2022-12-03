36fb64787f3f8fb386f4991fe88804136e5d9f29
Haeleon <matthewj52402@gmail.com>
2022-12-03 17:32:41 -0600

    Actually load a room or two

--- room-internal.h
+++ room-internal.h
@@ -2,6 +2,6 @@
 
 struct Room {
     char tiles[20][40];
-    char _raw_tiles[20][40];
+    // char _raw_tiles[20][40];
 //    int enemy_positions[smthn];
 };
--- roomgen.c
+++ roomgen.c
@@ -2,59 +2,42 @@
 #include "room-internal.h"
 #include "roomgen-index.h"
 
-Room temproom = {
-	{
-"##################    ##################",
-"#                #       #             #",
-"### # ### # # #####  ### # # # # ### # #",
-"#       #     #      #       #         #",
-"# # # ### # ### # #### # # ### # ### # #",
-"#     #     #            #       #     #",
-"# ### # # # # ### #  ### # # # ### # # #",
-"#   #         #            #           #",
-"### # ### ### # ###  # ### ### # #######",
-"    #     #       #  #             #    ",
-"    #     #       #  #             #    ",
-"# ### ### ### ### #  # # ##### # # ### #",
-"#   #   #   #     #    #   #   #       #",
-"### # ### # ### # #### ### # ##### # ###",
-"#   #                #     #   #   #   #",
-"### # # ### # # ###  ####### ### # ### #",
-"#               #    #     #   # #     #",
-"# ### # # # ### ########## ### # # # ###",
-"#               #                      #",
-"##################    ##################"
-	},
-	{
-"000000000000000000    000000000000000000",
-"0                0       0             0",
-"000 0 000 0 0 00000  000 0 0 0 0 000 0 0",
-"0       0     0      0       0         0",
-"0 0 0 000 0 000 0 0000 0 0 000 0 000 0 0",
-"0     0     0            0       0     0",
-"0 000 0 0 0 0 000 0  000 0 0 0 000 0 0 0",
-"0   0         0            0           0",
-"000 0 000 000 0 000  0 000 000 0 0000000",
-"    0     0       0  0             0    ",
-"    0     0       0  0             0    ",
-"0 000 000 000 000 0  0 0 00000 0 0 000 0",
-"0   0   0   0     0    0   0   0       0",
-"000 0 000 0 000 0 0000 000 0 00000 0 000",
-"0   0                0     0   0   0   0",
-"000 0 0 000 0 0 000  0000000 000 0 000 0",
-"0               0    0     0   0 0     0",
-"0 000 0 0 0 000 0000000000 000 0 0 0 000",
-"0               0                      0",
-"000000000000000000    000000000000000000"
-	}
-//    int enemy_positions[smthn];
-};
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Room *easy;
+Room *medium;
+Room *hard;
 
 void roomgen_pregen(){
-	//hi
+    FILE *levels = fopen("levels.txt", "r");
+    easy = (Room*) malloc(sizeof(Room) * NUM_EASY);
+    medium = (Room*) malloc(sizeof(Room) * NUM_MEDIUM);
+    hard = (Room*) malloc(sizeof(Room) * NUM_HARD);
+    int easyi = 0;
+    int mediumi = 0;
+    int hardi = 0;
+    char buffer[43];
+    while (fgets(buffer, 43, levels) != NULL) {
+        if (strlen(buffer) == 0) {
+            break;
+        }
+        Room *current_room =
+            buffer[0] == 'e' ? &easy[easyi++] :
+            (buffer[0] == 'm' ? &medium[mediumi++] :
+            &hard[hardi++]);
+        printf("%d %d %d\n", easyi, mediumi, hardi);
+        for(int y = 0; y < 20; ++y) {
+            fgets(buffer, 43, levels);
+            memcpy(&current_room->tiles[y], buffer, 40);
+        }
+    }
+    fclose(levels);
+    puts("Done!");
 }
 
 Room* roomgen_get(int x, int y){
-	// stub
-	return &temproom;
+    // stub
+    return easy;
 }
