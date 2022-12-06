f7a27bccd44e38f4093269fd2ab959a08a15a47a
Haeleon <matthewj52402@gmail.com>
2022-12-05 22:06:36 -0600

    Finish commenting roomgen.c

--- roomgen.c
+++ roomgen.c
@@ -9,20 +9,22 @@
 #include <math.h>
 #include <time.h>
 
-typedef Room *RoomPtr;
+typedef Room *RoomPtr; // so that I don't have to type **Room a legitimately painful amount of times
 
+// Rooms will be read from the text file into these arrays and shuffled
 RoomPtr easy[NUM_EASY];
 RoomPtr medium[NUM_MEDIUM];
 RoomPtr hard[NUM_HARD];
 
+// ...then put into this one to be returned as needed.
 RoomPtr grid[5][5];
 
 void roomgen_pregen(){
     if(DEBUG_MODE)
         puts("Reading file...");
 
-    FILE *levels = fopen("levels.txt", "r");
-    int easyi = 0;
+    FILE *levels = fopen("levels.txt", "r"); // Open file for reading
+    int easyi = 0; // Iterators for remembering which level we're on again?
     int mediumi = 0;
     int hardi = 0;
     char buffer[43];
@@ -46,28 +48,28 @@ void roomgen_pregen(){
             memcpy(&current_room->tiles[y], buffer, 40);
         }
     }
-    fclose(levels);
+    fclose(levels); // This line shuts the f[ile]
 
     if(DEBUG_MODE)
         puts("Shuffling levels...");
 
-    long seed = IS_SEED_SET?SET_SEED:time(NULL);
+    long seed = IS_SEED_SET?SET_SEED:time(NULL); // Get ready to randomize the thing y'all
     if(DEBUG_MODE)
         printf("Seed: %d\n", seed);
     srand(seed);
-    for(int i = NUM_EASY-1; i > 0; i--) {
+    for(int i = NUM_EASY-1; i > 0; i--) { // This randomly swaps each index in the array with one of the ones before it
         int rnum = rand() % i;
         Room *temp = easy[i];
         easy[i] = easy[rnum];
         easy[rnum] = temp;
     }
-    for(int i = NUM_MEDIUM-1; i > 0; i--) {
+    for(int i = NUM_MEDIUM-1; i > 0; i--) { // Same thing but with medium rooms
         int rnum = rand() % i;
         Room *temp = medium[i];
         medium[i] = medium[rnum];
         medium[rnum] = temp;
     }
-    for(int i = NUM_HARD-1; i > 0; i--) {
+    for(int i = NUM_HARD-1; i > 0; i--) { // ...yeah, you get it
         int rnum = rand() % i;
         Room *temp = hard[i];
         hard[i] = hard[rnum];
@@ -77,7 +79,7 @@ void roomgen_pregen(){
     if(DEBUG_MODE)
         puts("Creating level grid...");
     
-    RoomPtr new_grid[5][5] = {
+    RoomPtr new_grid[5][5] = { // This just fills the grid with levels. It is the only thing in this file that makes sense.
         {easy[0], hard[0], medium[0], hard[1], easy[1]},
         {hard[2], medium[1], easy[2], medium[2], hard[3]},
         {medium[3], easy[3], easy[4], easy[5], medium[4]},
@@ -86,18 +88,12 @@ void roomgen_pregen(){
     };
     for(int i = 0; i < 5; i++)
         for(int j = 0; j < 5; j++)
-            grid[i][j] = new_grid[i][j]; // C why...
-
-            /*
-EHMHE
-HMEMH
-MEEEM
-HMEMH
-EHMHE            */
-
+            grid[i][j] = new_grid[i][j]; // C, why...?
+    // Actually I take it back
 }
 
 Room* roomgen_get(int x, int y){
+    // Wrap the indexes from 0-4 then return the thing
     x += 2;
     y += 2;
     while(x<0) x += 5;
