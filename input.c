673bdccb96edbc38585d0f63e5a38be21c9889f0
GameChanger77 <gamer4christ2004@gmail.com>
2022-12-05 19:17:41 -0600

    comments

--- input.c
+++ input.c
@@ -4,7 +4,7 @@
 #include "input.h"
 
 int t = 0, Triangle = 0, Circle = 0, X = 0, Square = 0, JLX = 0, JLY = 0, JRX = 0, JRY = 0;
-int deadZone = 10, PJLX = 0, PJLY = 0, xCount = 0, yCount = 0, limitX = 1200, limitY = 2000;
+int deadZone = 30, PJLX = 0, PJLY = 0, xCount = 0, yCount = 0, limitX = 1200, limitY = 2000;
 
 void input_scan(){ // Scan in the input
 	scanf("%d, %d,%d,%d,%d, %d, %d, %d, %d ", &t, &Triangle, &Circle, &X, &Square, &JLX, &JLY, &JRX, &JRY);
@@ -21,6 +21,7 @@ Direction input_direction(){
 		exit(0);
 	}
 
+	// Add to the movement buffers
 	if(JLX > deadZone && PJLX <= deadZone) xCount = limitX;
 	if(JLX < -deadZone && PJLX >= -deadZone) xCount = -limitX;
 	if(JLY > deadZone && PJLY <= deadZone) yCount = limitY;
@@ -29,11 +30,13 @@ Direction input_direction(){
 	PJLX = JLX;
 	PJLY = JLY;
 
+	// Reset the movement buffers when the joystick is in the center
 	if (JLX < -deadZone || JLX > deadZone) xCount += JLX;
 	else xCount = 0;
 	if (JLY < -deadZone || JLY > deadZone) yCount += JLY;
 	else yCount = 0;
-	
+
+	// Return the direction to move
 	if (xCount > limitX){
 		xCount = 0;
 		return Right;
